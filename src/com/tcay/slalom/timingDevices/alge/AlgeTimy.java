784897769f8e5288d6d0a9438dbdd36eacd404de
amayers1 <allen.mayers@gmail.com>
2017-04-18 22:20:17 -0400

    Changed condition for warning message to only occur for stop times.

--- src/com/tcay/slalom/timingDevices/alge/AlgeTimy.java
+++ src/com/tcay/slalom/timingDevices/alge/AlgeTimy.java
@@ -32,6 +32,23 @@
  *     along with SlalomApp.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * This file is part of SlalomApp.
+ *
+ *     SlalomApp is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     SlalomApp is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with SlalomApp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.tcay.slalom.timingDevices.alge;
 
 //import com.tcay.slalom.Race;
@@ -213,7 +230,9 @@ public class AlgeTimy extends PhotoCellAgent {
 
                 }
                 else {
-                    msg.append(" NOT FOUND FOR FINISH (DNF?) !");
+                    if ((msgType & STOP_MASK) == STOP_MASK ) {
+                        msg.append(" NO RUN START FOUND FOR FINISH (DNF?) !");
+                    }
                 }
 
                 logRaw.info(msg.toString());
