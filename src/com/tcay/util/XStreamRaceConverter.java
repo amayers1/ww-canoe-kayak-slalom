e971460d1f467b8782e3d930e4ae197b9676c0ed
amayers1 <allen.mayers@gmail.com>
2017-04-18 22:15:22 -0400

    Added conversion method from Race Object to XML.

--- src/com/tcay/util/XStreamRaceConverter.java
+++ src/com/tcay/util/XStreamRaceConverter.java
@@ -15,9 +15,30 @@
  *     along with SlalomApp.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * This file is part of SlalomApp.
+ *
+ *     SlalomApp is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     SlalomApp is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with SlalomApp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.tcay.util;
 
+import com.tcay.slalom.BoatEntry;
+import com.tcay.slalom.Penalty;
 import com.tcay.slalom.Race;
+import com.tcay.slalom.RaceRun;
+import com.tcay.slalom.UI.JudgingSection;
 import com.thoughtworks.xstream.converters.Converter;
 import com.thoughtworks.xstream.converters.MarshallingContext;
 import com.thoughtworks.xstream.converters.UnmarshallingContext;
@@ -48,6 +69,10 @@ public class XStreamRaceConverter implements Converter {
             Race race  = (Race)value;
             writer.startNode("race");
 
+            writer.startNode("name");
+            writer.setValue(race.getName());
+            writer.endNode();
+
                writer.startNode("date");
                writer.setValue(race.getDate().toString());
                writer.endNode();
@@ -59,6 +84,145 @@ public class XStreamRaceConverter implements Converter {
             writer.startNode("nbrGates");
             writer.setValue(race.getNbrGates().toString());
             writer.endNode();
+
+
+
+            writer.startNode("judgingSections");
+            for (JudgingSection section:race.getSections()) {
+                writer.startNode("section");
+
+                writer.startNode("number");
+                writer.setValue(section.getSectionNbr().toString());
+                writer.endNode();
+
+
+                writer.startNode("startGate");
+                writer.setValue(Integer.toString(section.getFirstGate()));
+                writer.endNode();
+
+                writer.startNode("endGate");
+                writer.setValue(Integer.toString(section.getLastGate()));
+                writer.endNode();
+
+                writer.endNode();
+
+
+
+            }
+            writer.endNode();
+
+
+
+
+
+            writer.startNode("startList");
+
+            for (BoatEntry b:race.getRemainingStartList()) {
+                writer.startNode("boat");
+
+
+                writer.startNode("class");
+                writer.setValue(b.getBoatClass());
+                writer.endNode();
+
+
+                writer.startNode("bib");
+                writer.setValue(b.getRacer().getBibNumber());
+                writer.endNode();
+
+                writer.startNode("first");
+                writer.setValue(b.getRacer().getFirstName());
+                writer.endNode();
+
+                writer.startNode("last");
+                writer.setValue(b.getRacer().getLastName());
+                writer.endNode();
+
+                writer.startNode("club");
+                writer.setValue(b.getRacer().getClub());
+                writer.endNode();
+
+
+
+
+                writer.endNode();
+
+
+            }
+
+
+            writer.endNode();
+
+
+            writer.startNode("runsCompleted");
+
+            for (RaceRun run:race.getCompletedRuns()) {
+                writer.startNode("RaceRun");
+
+                writer.startNode("bib");
+                writer.setValue(run.getBoat().getRacer().getBibNumber());
+                writer.endNode();
+
+                writer.startNode("runNbr");
+                writer.setValue(Integer.toString(run.getRunNumber()));
+                writer.endNode();
+
+
+                writer.startNode("Time");
+
+                writer.startNode("Start");
+                writer.setValue(Long.toString(run.getStopWatch().getStartTime()));
+                writer.endNode();
+
+                writer.startNode("End");
+                writer.setValue(Long.toString(run.getStopWatch().getEndTime()));
+                writer.endNode();
+
+
+                writer.startNode("Penalties");
+
+                for (Penalty p:run.getPenaltyList()) {
+                    writer.startNode("Penalty");
+                    writer.startNode("Gate");
+                    writer.setValue(p.getGate().toString());
+                    writer.endNode();
+                    writer.startNode("Secs");
+                    writer.setValue(p.getPenaltySeconds().toString());
+                    writer.endNode();
+                    writer.endNode();
+                }
+
+                writer.endNode();
+
+
+
+
+
+                writer.startNode("RawTime");
+                writer.setValue(run.getResultString());
+                writer.endNode();
+
+
+                writer.startNode("TotalTime");
+                writer.setValue(run.getTotalTimeString());
+                writer.endNode();
+
+
+
+
+
+                writer.endNode();
+
+                writer.endNode();
+
+
+
+            }
+
+
+
+            writer.endNode();
+
 
 
 //            private List<Integer> upstreamGates;
