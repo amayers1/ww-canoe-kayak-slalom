00e7a94f014188f474cef36b41379e519a502ee7
amayers1 <allen.mayers@gmail.com>
2017-04-19 22:43:56 -0400

    Added class to track Photo Eye timing impulses.

---
+++ src/com/tcay/slalom/timingDevices/TimingImpulse.java
@@ -0,0 +1,59 @@
+/*
+ * This file is part of SlalomApp.
+ *
+ *     SlalomApp is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     SlalomApp is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with SlalomApp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This file is part of SlalomApp.
+ *
+ *     SlalomApp is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     SlalomApp is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with SlalomApp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.tcay.slalom.timingDevices;
+
+/**
+ * Created by allen on 4/18/17.
+ */
+public class TimingImpulse {
+
+
+    public enum PhotoEyePosition {
+        START,
+    //    SPLIT1,
+    //    SPLIT2,
+        FINISH
+    }
+
+
+    PhotoEyePosition location;
+    String time;
+
+
+    public TimingImpulse(PhotoEyePosition location, String time) {
+        this.location = location;
+        this.time = time;
+    }
+}
